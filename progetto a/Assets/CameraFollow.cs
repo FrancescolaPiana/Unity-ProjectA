04a0d20f61c55a7577feb3d79aeeca3d0295799b
So2enya <musumecithomas2@gmail.com>
2023-05-09 01:06:33 +0200

    Creazione ScenaThomas
    creazione scena thomas, camerafollow

---
+++ progetto a/Assets/CameraFollow.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    private Transform target;
+    private GameObject player;
+    public float smoothspeed = 7f;
+    public float Xmax;
+    public float Ymax;
+
+    private void Awake()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            target = player.transform;
+            if (player != null)
+            {
+                Debug.Log("preso");
+
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        
+
+        target = player.transform;
+
+        if (Mathf.Abs(target.position.x) < Xmax && Mathf.Abs(target.position.y) < Xmax) 
+        {
+
+
+
+            Vector3 targetposition = target.position - offset;
+            Vector3 smoothposition = Vector3.Lerp(transform.position, targetposition, smoothspeed * Time.deltaTime);
+            transform.position = smoothposition;
+        }
+
+        else if (Mathf.Abs(target.position.x) >= Xmax && Mathf.Abs(target.position.y) < Ymax)
+        {
+
+            Vector3 targetposition = new Vector3(transform.position.x, target.position.y, 0) - offset;
+            Vector3 smoothposition = Vector3.Lerp(transform.position, targetposition, smoothspeed * Time.deltaTime);
+            transform.position = smoothposition;
+        }
+
+        else if (Mathf.Abs(target.position.y) >= Ymax && Mathf.Abs(target.position.x) < Xmax)
+        {
+
+
+            Vector3 targetposition = new Vector3(target.position.x, transform.position.y, 0) - offset;
+            Debug.Log(targetposition);
+            Vector3 smoothposition = Vector3.Lerp(transform.position, targetposition, smoothspeed * Time.deltaTime);
+            transform.position = smoothposition;
+        }
+
+
+        //Poco smooth, da rivedere
+        //Aggiungere zoom in al buio
+    }
+}
